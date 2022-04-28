b77210dc1b8f3f290e7c6d85733ed544806b6d50
NightDreams <yayodcv@gmail.com>
2022-04-28 14:29:28 -0500

    c10 - Propiedades entre componentes: props

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import LoginForm from "./src/components/LoginForm";
+import Saludar from "./src/components/Saludo";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola DEVICE</Text>
-      <Text>Hola Mundo</Text>
-      <LoginForm />
+      <Text>Curso React Native </Text>
+      <Saludar name="oscar" />
+      <Saludar name="Luis" />
     </View>
   );
 }
---
+++ src/Saludar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Text } from "react-native";
+
+function Saludar({ name }) {
+  return <Text>Hola {name}</Text>;
+}
+
+export default Saludar;
